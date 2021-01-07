c7b2e247a18d5ee1b720f87ba2cd1e0325c79eaf
Lunaesky <landing.badji@outlook.com>
2021-01-07 19:53:40 +0100

    first commit

---
+++ safetalerts/src/main/java/net/safety/safetalerts/SafetalertsApplication.java
@@ -0,0 +1,13 @@
+package net.safety.safetalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetalertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetalertsApplication.class, args);
+	}
+
+}
